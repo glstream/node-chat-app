0a1239d64aa5fbec5de6c25bb52e0a7e8f7a5e9c
glstream <grayson.stream@gmail.com>
2017-04-27 10:55:04 -0700

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,22 +1,39 @@
-//require stuff
+//require native packages
 const path = require('path');
-const express = require('express');
+const http = require('http');
 
-//set up app variable
+//npm loaded packages
+const express = require('express');
+const socketIO = require('socket.io');
+
+//set up app, server and io vars
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //const for port 
 const port = process.env.PORT || 3000;
-//set exoress routes
+
+//set express routes
 const publicPath = path.join(__dirname+'/../public');
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+
+
 
 
 
 
 //set path for port
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App is running on ${port}`)
 });
