f40d05925de3527a962085193f58bc525db94782
glstream <grayson.stream@gmail.com>
2017-04-28 22:18:52 -0700

    create generateMessage util

--- server/server.js
+++ server/server.js
@@ -6,10 +6,15 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+//load local files
+const {generateMessage} = require('./utils/message')
+
 //set up app, server and io vars
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+
+
 
 //const for port 
 const port = process.env.PORT || 3000;
@@ -24,17 +29,9 @@ io.on('connection', (socket) => {
     
     // socket emit from admin 'welcome to the chat app..'
 
-    socket.emit('newMessage', { 
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New User Joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
   
 socket.on('createMessage', (message) => {
@@ -47,11 +44,7 @@ socket.on('createMessage', (message) => {
         console.log('createMessage', message)
 
 
-        // io.emit('newMessage', {
-        //     from:message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
+        io.emit('newMessage', generateMessage(message.from,message.text));
         // socket.broadcast.emit('newMessage', {
         //     from:message.from,
         //     text:message.text,
